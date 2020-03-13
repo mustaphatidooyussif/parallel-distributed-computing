31190733274305273c328e0d52fade97692a9978
Parrot User <user@localhost.localdomain>
2020-03-12 21:35:24 -0400

    .

--- LabExercise03/mr_top_k.py
+++ LabExercise03/mr_top_k.py
@@ -1,59 +1,100 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep 
 import re 
+from heapq import nlargest 
 
 WORD_RE = re.compile(r"[\w']+")
 
-
-stop_words ={'be', 'he', 'same', 'before', 'we', 'was', 'as', 
-            'out', 'hers', 'over', 'having', 'for', 'i', 'nor', 
-            'on', 'very', 'himself', 'now', 'she', 'until', 'are', 
-            'is', 'if', 'do', 'more', 'should', 'in', 'when', 
-            'these', 'between', 'to', 'our', 'which', 'or', 'off', 
-            'you', 'such', 'at', 'his', 'of', 'during', 'further', 
-            'so', 'by', 'where', 'ours', 'theirs', 'few', 'some', 
-            'a', 'about', 'yourselves', 'not', 'up', 'been', 'because',
-             'how', 'themselves', 'her', 'and', 'being', 'then', 
-             'did', 'why', 'all', 'both', 'but', 'itself', 'the', 
-             'under', 'too', 'yours', 'its', 'have', 'most', 'above', 
-             'down', 'only', 'my', 'from', 'me', 'this', 'here', 'whom', 
-             'yourself', 'through', 'am', 'has', 'while', 'herself', 
-             'other', 'them', 'what', 'just', 'there', 'than', 'can',
-              'any', 'don', 'myself', 'that', 'no', 'who', 'each', 'will',
-               'him', 'it', 'their', 'below', 'own', 'had', 'after', 
-               'ourselves', 'into', 'doing', 'they', 'those', 'your',
-                'against', 'again', 'with', 'once', 'were', 'does', 'an'}
 
 class MRTopK(MRJob):
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_get_words,
+            MRStep(mapper_init = self.mapper_init,
+                   mapper=self.mapper_get_words,
                    combiner = self.combiner_count_words,
                    reducer=self.reducer_count_words),
             MRStep(reducer=self.reducer_find_max_word)
         ]
 
+        
+    def configure_args(self):
+        """
+        This method configures commandline 
+        options for the program. 
+        The commandline options:
+            --stop-words: the name of the file 
+            containing the stop words. 
+        """
+        super(MRTopK, self).configure_args()
+        self.add_passthru_arg(
+            "--top-k", default = 3,
+            help = "specify the top k value. "
+        )
+        self.add_file_arg(
+            "--stop-words", default = "def_stop_words.txt",
+            help = "specify the stop words file. "
+        )
+
+    def mapper_init(self):
+        """
+        This method initializes or setup 
+        the resources needed by the mapper function. 
+        """
+        file = self.options.stop_words
+        self.stopwords_set = None
+        with open(file, "r") as f:
+            words = [ i.strip().lower() for i in f.read().split()]
+
+        self.stopwords_set = set(words)
+
     def mapper_get_words(self, _, line):
+        """
+        This method splits every sentence from 
+        the text file is split into words. All
+        stop words are excuded and the finally 
+        the a key-value pairs of (word, 1) are 
+        returned. 
+        """
         #yield each word in the line.
         for word in WORD_RE.findall(line):
-            if word not in stop_words:
+            if word not in self.stopwords_set:
                 yield (word.lower(), 1)
 
     def combiner_count_words(self, word, counts):
+        """
+        This method sums the number of words seen 
+        so far. The number of words seen so far 
+        might to be the totaal number of words 
+        from the file, but the subset of the words
+        from the file. 
+        :param word: the key which is the word. 
+        :param counts: the number of times `word`
+                      appeared so far. 
+        """
         #sum all the words we've seen so far
         yield(word, sum(counts))
 
     def reducer_count_words(self, word, counts):
-        #send all(num_occurances, word) pairs to the same reduce
-        #num_occurances is so  we can easily use python's max() function. 
+        """end all(num_occurances, word) pairs to 
+        the same reduce num_occurances is so  we 
+        can easily use python's max() function. 
+        """
 
         yield None, (sum(counts), word)
 
     def reducer_find_max_word(self, _, word_count_pairs):
         #each item of word_count_pair is (count, word)
         #so yielding one results in key=count, value=word
-        yield max(word_count_pairs)
+        yield None, word_count_pairs
+        
+        for count_word in word_count_pairs:
+            top_k = nlargest(self.options.top_k, count_word)
+
+        for i in range(int(self.options.top_k)):
+            yield top_k
+
+        #yield max(word_count_pairs)
 
 
 if __name__=="__main__":
--- LabExercise03/mr_word_count.py
+++ LabExercise03/mr_word_count.py
@@ -1,35 +1,70 @@
+"""
+Author: MUutapha Tidoo YUssif
+Lab Exercise 03. 
+"""
+
 from mrjob.job import MRJob 
 from mrjob.step import MRStep 
 import re 
 import os 
+import config  
 
 WORD_RE = re.compile(r"[\w']+")
 
 class MRWordCount(MRJob):
-
+    """
+    This program counts the number of 
+    occurances of words in a text file 
+    passed. 
+    """
     def configure_args(self):
+        """
+        This method configures commandline 
+        options for the program. 
+        The commandline options:
+            --stop-words: the name of the file 
+            containing the stop words. 
+        """
         super(MRWordCount, self).configure_args()
-        self.add_passthru_arg(
+        self.add_file_arg(
             "--stop-words", default = "def_stop_words.txt",
             help = "specify the stop words file. "
         )
 
     def mapper_init(self):
-        self.stop_words_set = None 
-        stop_words_file = self.options.stop_words
-        with open("/home/user/Desktop/Ashesi/2020/parallel-distributed-computing/LabExercise03/" + stop_words_file, "r") as f:
+        """
+        This method initializes or setup 
+        the resources needed by the mapper function. 
+        """
+        file = self.options.stop_words
+        self.stopwords_set = None
+        with open(file, "r") as f:
             words = [ i.strip().lower() for i in f.read().split()]
 
-        self.stop_words_set =  set(words)
+        self.stopwords_set = set(words)
 
     def mapper(self, _, line):
-
+        """
+        This method splits every sentence from 
+        the text file is split into words. All
+        stop words are excuded and the finally 
+        the a key-value pairs of (word, 1) are 
+        returned. 
+        """
         #yield each non stopword in the line.
         for word in WORD_RE.findall(line):
-            if word not in self.stop_words_set:
+            if word not in self.stopwords_set:
                 yield (word.lower(), 1)
 
     def reducer(self, word, counts):
+        """
+        This method aggregates the ouput from
+        the mapper to do the final work. It takes
+        key-value pairs as an input and returns 
+        sum of the values. THe values are the 
+        subset of the number of times the key
+        appears in the text. 
+        """
         #sum all the words we've seen so far
         yield(word, sum(counts))
 
